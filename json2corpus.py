eb8eefdf660bc1de65f6c5d17fa508c366a25ccf
Shantanu Nair <nairs@purdue.edu>
2018-12-10 06:53:48 -0500

    implementing json2corpus

--- json2corpus.py
+++ json2corpus.py
@@ -1,9 +1,26 @@
 import json
+import os
 from pprint import pprint
+from io import open
 
+abstracts = ''
+
+def preprocessText(text):
+    removeNewlines(text)
+
+def removeNewlines(text):
+    text.replace("\n", "")
 
 for i in range(24,40):
-    with open('data.json') as f:
-    data = json.load(f)
+    for filename in os.listdir('year'+str(i)):
+        print(filename)
+        if filename.endswith('.json'):
+            with open('year'+str(i)+'/'+filename) as f:
+                data = json.load(f)
+                abstract = data['abstract']
+                # pprint(data)
+                abstracts = abstracts + abstract + '\n'
+print(abstracts)
+corpus = open('abstracts.cor',mode="w",encoding="utf-8")
+corpus.write(abstracts)
 
-    pprint(data)
---
+++ par2vec.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import gensim
+import os
+import collections
+import smart_open
+import random
+
+# Set file names for train and test data
+test_data_dir = '{}'.format(os.sep).join([gensim.__path__[0], 'test', 'test_data'])
+lee_train_file ='abstracts.cor'
+lee_test_file = test_data_dir + os.sep + 'lee.cor'
+print("corpus: " + lee_train_file)
+def read_corpus(fname, tokens_only=False):
+    with smart_open.smart_open(fname, encoding="iso-8859-1") as f:
+        for i, line in enumerate(f):
+            if tokens_only:
+                yield gensim.utils.simple_preprocess(line)
+            else:
+                # For training data, add tags
+                yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
+
+train_corpus = list(read_corpus(lee_train_file))
+test_corpus = list(read_corpus(lee_test_file, tokens_only=True))
+
+
+model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+model.build_vocab(train_corpus)
+model.infer_vector(['only', 'you', 'can', 'prevent', 'forest', 'fires'])
+
+
+ranks = []
+second_ranks = []
+for doc_id in range(len(train_corpus)):
+    inferred_vector = model.infer_vector(train_corpus[doc_id].words)
+    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+    rank = [docid for docid, sim in sims].index(doc_id)
+    ranks.append(rank)
+    
+    second_ranks.append(sims[1])
+collections.Counter(ranks)  # Results vary between runs due to random seeding and very small corpus
+
+print('Document ({}): {}\n'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
+print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
+for label, index in [('MOST', 0), ('SECOND-MOST', 1), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
+    print(u'%s %s: %s\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
+    # Pick a random document from the corpus and infer a vector from the model
+doc_id = random.randint(0, len(train_corpus) - 1)
+
+# Compare and print the second-most-similar document
+print('Train Document ({}): «{}»\n'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
+sim_id = second_ranks[doc_id]
+print('Similar Document {}: «{}»\n'.format(sim_id, ' '.join(train_corpus[sim_id[0]].words)))
+
+
+# Pick a random document from the test corpus and infer a vector from the model
+doc_id = random.randint(0, len(test_corpus) - 1)
+inferred_vector = model.infer_vector(test_corpus[doc_id])
+sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+
+# Compare and print the most/median/least similar documents from the train corpus
+print('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(test_corpus[doc_id])))
+print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
+for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
+    print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
